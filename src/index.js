b6d1a0cec150dc4a1934425f6e4386090070027d
dunghoang168 <dunghoang16082003@gmail.com>
2024-06-05 14:47:33 +0700

    detailProduct-reviewPage-contactPage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HomePage from "./pages/homePage";
+//import HomePage from "./pages/homePage";
 import { BrowserRouter } from 'react-router-dom'
 import RouterCustom from "./router";
 import './style/style.scss';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <RouterCustom />
-    </BrowserRouter>
+            <BrowserRouter>
+                <RouterCustom>
+                </RouterCustom>
+            </BrowserRouter>
 );
 
